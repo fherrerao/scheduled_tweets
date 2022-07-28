28902545ab2dfc1fa85a5a57bb8cf9cc3395db53
fherrerao <fherrera0206@gmail.com>
2022-07-28 08:42:03 -0500

    Add routes to sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   resources :about, only: [:index]
   resources :main, only: [:index]
-  root "main#index"
-  get "signup", to: "registration#new"
-  post "signup", to: "registration#create"
+  root 'main#index'
+  get 'signup', to: 'registration#new'
+  post 'signup', to: 'registration#create'
+  delete 'logout', to: 'sessions#destroy'
+
+  get 'sign_in', to: 'sessions#new'
+  post 'sign_in', to: 'sessions#create'
 end
