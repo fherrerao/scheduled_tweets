259f4689c8177fa11b6f850f0e8fae984d980703
fherrerao <fherrera0206@gmail.com>
2022-07-28 15:01:05 -0500

    Add Current model to check the user in every request

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   has_secure_password
 
   validates :email, presence: true, uniqueness: true,
-                    format: { with: /\A[^@\s]+@[^@\s]+\.[^@]+\z/, message: 'must be a valid email address' }
+                    format: { with: /\A[^@\s]+@[^@\s]+\.[^@]+\z/, message: 'Must be a valid email address' }
   validates :password, presence: true, length: { minimum: 6 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,17 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  resources :about, only: [:index]
-  resources :main, only: [:index]
   root 'main#index'
-  get 'signup', to: 'registration#new'
-  post 'signup', to: 'registration#create'
-  delete 'logout', to: 'sessions#destroy'
+  resources :about, only: [:index]
+  resources :main, only: [:index]  
+  
+  get 'sign_up', to: 'registration#new'
+  post 'sign_up', to: 'registration#create'
+
+  get 'passwords', to: 'passwords#edit', as: :edit_password
+  patch 'passwords', to: 'passwords#update'
 
   get 'sign_in', to: 'sessions#new'
   post 'sign_in', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 end
