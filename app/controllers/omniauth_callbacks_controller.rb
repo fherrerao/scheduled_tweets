161d10ff4ec5f1e03b30f502023a7bdd4f29e5a2
fherrerao <fherrera0206@gmail.com>
2022-08-03 11:51:54 -0500

    Get the personal data from twitter account

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,5 +1,19 @@
 class OmniauthCallbacksController < ApplicationController
+  before_action :login_required!
+
   def twitter
-    render plain: 'Success!'
+    twitter_account = Current.user.twitter_accounts.where(username: auth.info.nickname).first_or_initialize
+    twitter_account.update(
+      name: auth.info.name,
+      username: auth.info.nickname,
+      image: auth.info.image,
+      token: auth.info.token,
+      secret: auth.info.secret,
+    )
+    redirect_to root_path, notice: "Succesfully connected your Twitter account"
+  end
+  
+  def auth
+    request.env['omniauth.auth']
   end
 end
