d12701b67c333d6e834e907d52af879280b31f88
fherrerao <fherrera0206@gmail.com>
2022-08-06 21:13:23 -0500

    Add associations between user and twitter account

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,11 @@
 
 class User < ApplicationRecord
   has_many :twitter_accounts
+  has_many :tweets
   has_secure_password
 
-  validates :email, presence: true, uniqueness: true,
-                    format: { with: /\A[^@\s]+@[^@\s]+\.[^@]+\z/, message: 'Must be a valid email address' }
   validates :password, presence: true, length: { minimum: 6 }
+  validates :email, presence: true, uniqueness: true,
+                    format: { with: /\A[^@\s]+@[^@\s]+\.[^@]+\z/, 
+                    message: 'Must be a valid email address' }
 end
