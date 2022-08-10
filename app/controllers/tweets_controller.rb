0b1e3ff6654020ab5982f0f5a4f0e595e6ebf2ab
fherrerao <fherrera0206@gmail.com>
2022-08-09 21:41:34 -0500

    Add methods, create, edit and destroy for tweets

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,11 +1,50 @@
 class TweetsController < ApplicationController
   before_action :login_required!
-  
+  before_action :set_tweet, only: [:show, :edit, :update, :destroy]
+
   def index
-    @tweet = Current.user.tweets
+    @tweets = Current.user.tweets
   end
 
   def new
-    
+    @tweet = Tweet.new
+  end
+
+  def create
+    @tweet = Current.user.tweets.new(tweet_params)
+    if @tweet.save
+      redirect_to tweets_path, notice: "Tweet was scheduled succesfully"
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @tweet.update(tweet_params)
+      redirect_to tweets_path, notice: "Tweet was updated succesfully!"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @tweet.destroy
+      redirect_to tweets_path, notice: "Tweet was destroyed succesfully"
+    else
+      render :tweet
+    end
+  end
+
+  private
+
+  def tweet_params
+    params.require(:tweet).permit(:twitter_account_id, :body, :publish_at)
+  end
+
+  def set_tweet
+    @tweet = Current.user.tweets.find(params[:id])
   end
 end
